8bc940789484fb7d32d02795c061f31cf783c599
haxorsprogramming <haxorsuinsu@gmail.com>
2021-02-12 12:35:41 +0700

    update

--- Diana_Django_Project/settings.py
+++ Diana_Django_Project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'login',
+    'login'
 ]
 
 MIDDLEWARE = [
@@ -118,4 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ladun/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "ladun",
+]
+
