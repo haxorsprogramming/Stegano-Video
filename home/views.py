327bdf83cee2f4b250b5b7c043b19bb5431064a7
haxorsprogramming <haxorsuinsu@gmail.com>
2021-02-12 18:20:48 +0700

    update

--- home/views.py
+++ home/views.py
@@ -6,25 +6,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+import hashlib
+
 # Create your views here.
 def home_page(request):
     count = 0
-    videoFile = "ladun/data_uji/nature.mp4"
+    videoFile = "ladun/data_uji/lulu.mp4"
     cap = cv2.VideoCapture(videoFile)
     frameRate = cap.get(5)
     x = 1
-    while(cap.isOpened()):
-        idFrame = cap.get(1)
-        ret, frame = cap.read()
-        if(ret != True):
-            break
-        if(idFrame % math.floor(frameRate) == 0):
-            filename = "keras_proses/frame_%d_.jpg" % count; count+=1
-            cv2.imwrite(filename, frame)
+    # while(cap.isOpened()):
+    #     idFrame = cap.get(1)
+    #     ret, frame = cap.read()
+    #     if(ret != True):
+    #         break
+    #     if(idFrame % math.floor(frameRate) == 0):
+    #         filename = "keras_proses/frame_%d_.jpg" % count; count+=1
+    #         cv2.imwrite(filename, frame)
 
-    cap.release()
-    
+    # cap.release()
+    pesan = hidden_message(videoFile)
+    print(pesan)
     context = {
         'status' : 'sukses'
     }
     return render(request, 'home/home.html', context)
+
+
+def hidden_message(filename):
+    h = hashlib.sha1()
+    with open(filename, 'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h.update(chunk)
+    
+    return h.hexdigest()
+
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Encode_Pesan(models.Model):
+    kd_uji = models.CharField(max_length=150)
+    nama_video = models.CharField(max_length=150)
+    nama_pengujian = models.CharField(max_length=150)
+    rsa = models.CharField(max_length=150)
+    rsa_crt = models.CharField(max_length=150)
+    a_value = models.IntegerField()
+    c_value = models.IntegerField()
+    m_value = models.IntegerField()
+    x_0 = models.CharField(max_length=150)
+    crt_value = models.FloatField()
+    waktu_pengujian = models.DateTimeField()
+    message_encode = models.CharField(max_length=200)
+
+class Decode_Pesan(models.Model):
+    kd_uji = models.CharField(max_length=150)
+    nama_video = models.CharField(max_length=150)
+    nama_pengujian = models.CharField(max_length=150)
+    rsa_public = models.CharField(max_length=150)
+    rsa_crt = models.CharField(max_length=150)
+    a_value = models.IntegerField()
+    c_value = models.IntegerField()
+    m_value = models.IntegerField()
+    x_0 = models.CharField(max_length=150)
+    crt_value = models.FloatField()
+    waktu_pengujian = models.DateTimeField()
+    message_decode = models.CharField(max_length=200)
+
+class Video_Proses(models.Model):
+    kd_uji = models.CharField(max_length=150)
+    frame = models.CharField(max_length=10)
+    hash_data = models.CharField(max_length=200)
